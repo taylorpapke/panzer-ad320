fc5e83b23632be68f218dbd802d13220ef8a3feb
Json Panzer <jason.drew.panzer@gmail.com>
2022-02-03 19:54:13 -0800

    Started our notecard app

---
+++ client/src/components/CardNavigation/CardNavigation.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './CardNavigation.css'
+
+function CardNavigation() {
+  return (
+    <div className="card-nav">
+      <ul>
+        <li>Card A</li>
+        <li>Card A</li>
+        <li>Card A</li>
+        <li>Card A</li>
+        <li>Card A</li>
+        <li>Card A</li>
+        <li>Card A</li>
+        <li>Card A</li>
+        <li>Card A</li>
+        <li>Card A</li>
+        <li>Card A</li>
+      </ul>
+    </div>
+  )
+}
+
+export default CardNavigation
