d79334e515437d2aacc88133b43f4ba14ff806b7
Json Panzer <jason.drew.panzer@gmail.com>
2022-02-10 16:04:00 -0800

    working deck cards route

--- service/app.js
+++ service/app.js
@@ -1,10 +1,20 @@
 import express from 'express'
+import mongoose from 'mongoose'
 import cors from 'cors'
 
-import Deck from './models/Deck'
+import { Deck } from './models/Deck.js'
 
 const app = express()
 const port = 8000
+
+// Connect to MongoDB
+
+const connectionString = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@crit-cluster.bpw1p.mongodb.net/notoriety?retryWrites=true&w=majority`
+try {
+  await mongoose.connect(connectionString)
+} catch (err) {
+  console.log('error ', err)
+}
 
 // Middleware
 
@@ -15,7 +25,7 @@ const exampleMiddleware = (req, res, next) => {
 
 app.use(cors())
 app.use(express.json())
-app.use(exampleMiddleware())
+app.use(exampleMiddleware)
 
 // Routes
 
@@ -23,13 +33,13 @@ app.get('/', (req, res) => {
   res.send('Hello, world!')
 })
 
-const cardsByDeck = (req, res) => {
+const cardsByDeck = async (req, res) => {
   const cards = await Deck.findById(req.params.id)
   console.log(`cards found ${cards.length}`)
   res.json(cards)
 }
 
-app.get('/deck/:id/cards', cardsByDeck)
+app.get('/decks/:id/cards', cardsByDeck)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
