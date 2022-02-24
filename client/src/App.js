53bbe21ba6af46f7e96bc8d288a4524e9d98338f
Json Panzer <jason.drew.panzer@gmail.com>
2022-02-23 21:34:45 -0800

    Update components for new data model

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,27 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Container } from '@mui/material'
 import './App.css'
 import Topbar from './components/Topbar/Topbar'
 import DeckProvider from './components/Deck/DeckProvider'
+import axios from 'axios'
 
 function App() {
   const [createMode, setCreateMode] = useState(false)
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    axios.get('http://localhost:8000/users').then((response) => {
+      console.log(`response from users ${response.data[0].firstName}`)
+      setUser(response.data[0])
+    })
+  }, [])
 
   return (
     <React.Fragment>
       <Topbar createCardHandler={() => { setCreateMode(true) }} />
-      <Container width="md">
-        <DeckProvider deckId="620d8ea5fced0e15829e5d35" createMode={createMode} endCreate/>
+      <Container width="lg">
+        {user === null ? <span>Loading...</span> :
+          <DeckProvider decks={user.decks} createMode={createMode} /> }
       </Container>
     </React.Fragment>
   )
--- client/src/components/Flashcard/FlashcardProvider.js
+++ client/src/components/Flashcard/FlashcardProvider.js
@@ -1,59 +1,51 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
-import { Stack } from '@mui/material'
+import React, { useState } from "react"
+import { List, ListItemButton, ListItemText, Paper, Stack } from "@mui/material"
 
-import Flashcard from './Flashcard'
-import FlashcardList from './FlashcardList'
+import Flashcard from "./Flashcard"
 
-const FlashcardProvider = ({ deckId }) => {
-  const [cards, setCards] = useState(null)
+const FlashcardProvider = ({ deck }) => {
   const [index, setIndex] = useState(0)
-
-  useEffect(() => {
-    axios
-      .get(`http://localhost:8000/decks/${deckId}/cards`)
-      .then(response => {
-        const data = response.data
-        console.log('[FlashcardProvider] response ', data)
-          setCards(data)
-      })
-      .catch(err => {
-        console.log('error in FlashcardProvider', err)
-      })
-  }, [deckId])
 
   const previous = () => {
     if (index > 0) {
-      setIndex(index-1)
+      setIndex(index - 1)
     }
   }
 
   const next = () => {
-    if (index < cards.length - 1) {
-      setIndex(index+1)
+    if (index < deck.cards.length - 1) {
+      setIndex(index + 1)
     }
   }
 
-  const selectIndex = (selectedIndex) => {
-    setIndex(selectedIndex)
-  }
-
-  if (!cards) {
-    return <span>Loading...</span>
-  } else {
-    console.log('[Flashcard Provider] length of cards ', cards.length)
-    return (
-      <Stack
+  console.log("[Flashcard Provider] length of cards ", deck.cards.length)
+  return (
+    <Stack
       direction="row"
       justifyContent="space-around"
       alignItems="flex-start"
-        spacing={2}
-        sx={{ mt: 3 }}
-      >
-        <FlashcardList cards={cards} selectedIndex={index} selectIndexHandler={selectIndex} /> 
-        <Flashcard card={cards[index]} previous={previous} next={next} />
-      </Stack>
-    )
-  }
+      spacing={2}
+      sx={{ mt: 3 }}
+    >
+      <Paper elevation={3}>
+        <List sx={{ width: "15vw" }}>
+          {deck.cards.map((card, idx) => {
+            return (
+              <ListItemButton
+                key={idx}
+                selected={idx === index}
+                onClick={() => {
+                  setIndex(idx)
+                }}
+              >
+                <ListItemText primary={`${card.frontText.slice(0, 15)}...`} />
+              </ListItemButton>
+            )
+          })}
+        </List>
+      </Paper>
+      <Flashcard card={deck.cards[index]} previous={previous} next={next} />
+    </Stack>
+  )
 }
 export default FlashcardProvider
